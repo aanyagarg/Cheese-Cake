afc9e5a86348e71fa0eeb5fff829bea8f69b4035
aanyagarg <73021607+aanyagarg@users.noreply.github.com>
2020-12-15 08:30:17 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+//6 tablespoons unsalted butter
+
+//1 1/2 cups graham cracker crumbs
+
+//2 tablespoons granulated sugar
+
+//Pinch fine salt
+//Filling:
+
+//2 pounds cream cheese, at room temperature
+
+//1 1/4 cups granulated sugar
+
+//1 1/4 cups sour cream
+
+//6 large eggs, lightly beaten
+
+//1 tablespoon vanilla paste (see Cook's Note) or extract
+
+//1 teaspoon finely grated lemon zest
+
+//1 teaspoon finely grated orange zest
+
+//Topping:
+
+//3/4 cup sour cream
+
+//1/2 cup confectioners' sugar
+
+//1/4 teaspoon vanilla paste or extract
+
+//Berries, optional
---
+++ Recipe.js
@@ -0,0 +1,28 @@
+//Take 10 to 12 digestive biscuits or crackers or marie biscuits. Break them and add in a blender or grinder jar.
+//Grind to a fine powder. take this powder in a mixing bowl.
+//Add 2 tablespoons melted butter in it. mix very well with a spoon.
+//Now place this mixture in greased spring pan. Grease the pan very well with butter. 
+
+//Making Cheesecake
+//In another bowl, take 3 cups cream cheese (600 grams), ¾ cup sugar (150 grams), 1 tablespoon corn starch, 1 tablespoon lemon juice and 1 teaspoon vanilla extract or powder.
+//Now with an electric beater, start to beat this mixture.
+//Use a medium to medium-high speed and whip till smooth. Do not over beat. 
+//Now add ½ cup cream.
+//Again beat the mixture till the cream is blended very well and you see a homogenous, creamy, thick smooth mixture. Do not over beat. 
+//Now pour the whipped cream cheese layer in the pan. with a spatula even the top.
+//Keep the pans in a preheated oven and bake for 30 to 40 minutes. Preheat oven at 180 degrees celsius for 15 minutes and keep both the top and bottom heating elements on. Keep the pans in the middle rack.
+//To check the doneness, the sides will look firm, risen and the center of the cheesecake will be wobbly. The sides will also look separated from the pan. Let the cheesecake cool at room temperature.
+//Then cover with a foil or an air tight lid and place the pans in the fridge for 4 hours or more. You can also keep overnight.
+
+//For Chocolate Sauce
+//Take ½ cup hot milk in a bowl. You can heat milk in a pan or a microwave. No need to boil the milk. Just let it get hot.
+//Add 1 cup roughly chopped sweet chocolate (75 grams). If using semi sweet or bitter chocolate, then do add sugar as per taste.
+//Mix very well with a spoon or spatula till all the chocolate dissolves.
+//Let the chocolate sauce cool at room temperature.
+//Removing Cheesecake From The Pan
+//Dip a butter knife in a bowl or cup containing warm water.
+//Now take the butter knife and gently slid through the sides of the pan, separating the cheesecake from the pan.
+//Push the detachable lid from the bottom and gently remove the cheesecake. Then remove the lid from the base.
+//Place the cheesecake on a serving plate or tray.
+//Drizzle the chocolate sauce and serve. Or your family or guests can serve the chocolate sauce themselves on the sliced cheesecakes. If you want you can even add some berries like blueberries, blackberries or strawberries while serving. Dry fruits can also be sprinkled on the cheesecake.
+
